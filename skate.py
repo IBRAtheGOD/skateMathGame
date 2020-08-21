ec4a2b7110e0157342b6f1a614b3c4c2b3f4de17
Ian Jones <iansnachos@hotmail.com>
2020-08-20 21:15:39 -0400

    h

--- skate.py
+++ skate.py
@@ -25,11 +25,32 @@ class Player:
 #creates instance of the class player. player1 is an object
 player1 = Player
 #takes input to assign a name to the player
-player1.name = raw_input("Please enter your name: ")
+player1.name = input("Please enter your name: ")
 #now we ask the user their favorite tricks
-player.favoriteTrick1 = raw_input("\nGreat! Now please enter your top 3 favorite skateboard tricks: ")
-player.favoriteTrick2 = raw_input("\n")
-player.favoriteTrick3 = raw_input("\n")
-#test print
-print(f'your name is {player1.name} and your top 3 favorite skateboard tricks are the {player1.favoriteTrick1}\n, the {player1.favoriteTrick2}, and the {player1.favoriteTrick3}')
+player1.favoriteTrick1 = input("\nGreat! Now please enter your top 3 favorite skateboard tricks => ")
+player1.favoriteTrick2 = input("\n=>")
+player1.favoriteTrick3 = input("\n=>")
+# test print
+# print(f'your name is {player1.name} and your top 3 favorite skateboard tricks are the {player1.favoriteTrick1}\n, the {player1.favoriteTrick2}, and the {player1.favoriteTrick3}')
+print("\n##########################################")
+print("\n#  ------  |  /   ----   -----  -----    #")
+print("\n#  |       | /   |    |    |    |        #")
+print("\n#  ------  |/    ------    |    -----    #")
+print("\n#       |  |\\   |    |    |    |        #")
+print("\n#  ------  | \\  |    |    |    -----    #")
+print("\n##########################################")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
